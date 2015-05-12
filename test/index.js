be52cd73742b4130bf0e890d629e33f1f2c3e452
JMontupet <jeremy@icysoft.fr>
2015-05-12 20:47:34 +0200

    First attempt to call a remote service through ZMQ
    - add: client base
    - add: server base
    - add: one basic test
    - add: basic router

---
+++ test/index.js
@@ -0,0 +1,33 @@
+﻿var assert = require('assert');
+var ZMQomClient = require('./../lib/ZMQomClient');
+var ZMQomServer = require('./../lib/ZMQomServer');
+
+
+var testClient;
+var testServer;
+before(function (done) {
+    // Test Server
+    testServer = new ZMQomServer('tcp://127.0.0.1:3000');
+    testServer.req('/echo', function (req, res, next) {
+        res.body = req.data;
+        return next();
+    });
+
+    // Test Client
+    testClient = new ZMQomClient('tcp://127.0.0.1:3000');
+    
+    // TODO: Emit event when client/server is ready
+    setTimeout(function () {
+        done();
+    }, 200);
+});
+
+describe('Basic Tests', function () {
+    it('Test echo', function (done) {
+        testClient.req('/echo', 'Hello !', function (err, data) {
+            if (err) throw err;
+            assert.equal(data, 'Hello !', 'Echo not equals to request');
+            done();
+        });
+    });
+});
